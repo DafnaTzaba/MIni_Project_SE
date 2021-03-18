50f84370791a587dbf21330202d62b87ee3d36d5
da077 <da077@DESKTOP-HV19KNK>
2021-03-18 12:02:17 +0200

    לאט לאט

--- src/primitives/Point3D.java
+++ src/primitives/Point3D.java
@@ -24,7 +24,8 @@ public Point3D(Coordinate a,Coordinate b,Coordinate c)
 	y=b;
 	z=c;
 }
-public Point3D(Point3D a)
+
+public Point3D(Point3D a) //copy constructor
 {
 	x=a.x;
 	y=a.y;
@@ -32,13 +33,32 @@ public Point3D(Point3D a)
 }
 
 
+/**
+ * primary constructor for Point3D
+ * @param x coordinate value for X axis
+ * @param y coordinate value for Y axis
+ * @param z coordinate value for Z axis
+ */
+public Point3D(double a, double b, double c)
+{
+    x = new Coordinate(a);
+    y = new Coordinate(b);
+    z = new Coordinate(c);
+}
+
+
+
+
+
 @Override
 public boolean equals(Object obj) {
    if (this == obj) return true;
-   if (obj == null) return false;
+   if (obj == null ) return false;
    if (!(obj instanceof Point3D)) return false;
    Point3D other = (Point3D)obj;
    return this.x.equals(other.x) && this.y.equals(other.y) && this.z.equals(other.z);
 }
+
+
 
 }
--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,29 +1,44 @@
 package primitives;
 
+
 public class Vector {
 	 private Point3D head;
-
-	
 
 	public Point3D getHead() {
 		return head;
 	}
 
 	//Constructors///
-	
+public Vector(double x, double y, double z) 
+{        
+	this(new Point3D(x, y, z));	
+}	
+
 public Vector(Point3D h)
 {
+	Point3D zero =new Point3D(0,0,0);
+    if (head.equals(zero)) 
+        throw new IllegalArgumentException("Vector head cannot be Point(0,0,0)");
+
 	head=new Point3D(h);
-	}
-public Vector(Vector b) {
+}
+
+public Vector(Vector b) //copy constructor
+{
 	head=new Point3D(b.head);
 }
 
 @Override
-public boolean equals(Object obj) {
+public boolean equals(Object obj)
+{
+	   if (this == obj) return true;
+       if (obj == null || getClass() != obj.getClass()) return false;
+       Vector vector = (Vector) obj;
+       return head.equals(vector.head);
    
-   return head.equals(obj) ;
 }
+
+
 
 }
 
