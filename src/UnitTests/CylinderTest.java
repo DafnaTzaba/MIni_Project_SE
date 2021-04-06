6b646bb17263ddab016c338b5ac2578e5ea172ed
da077 <da077@DESKTOP-HV19KNK>
2021-04-07 00:19:39 +0300

    ה' ירחם

---
+++ src/UnitTests/CylinderTest.java
@@ -0,0 +1,18 @@
+package UnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CylinderTest {
+
+	@Test
+	public void testGetNormal() {
+
+	}
+
+	
+
+
+
+}
---
+++ src/UnitTests/TubeTest.java
@@ -0,0 +1,19 @@
+package UnitTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TubeTest {
+
+	@Test
+	public void testTube() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNormal() {
+		fail("Not yet implemented");
+	}
+
+}
