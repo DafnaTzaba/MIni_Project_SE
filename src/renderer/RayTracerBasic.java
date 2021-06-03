9fd67ed4faeb1cb499af17e8726678599002c4e3
heroot12@gmail.com <heroot12@gmail.com>
2021-06-03 10:01:23 +0300

    cdscs

--- src/renderer/RayTracerBasic.java
+++ src/renderer/RayTracerBasic.java
@@ -244,7 +244,7 @@ public class RayTracerBasic extends RayTracerBase {
 	
 	//////// Soft Shadow////////////////////////////
 	 /**
-     * temp function fdgfdgd
+     * temp function fdgfdgdcdcd
      * create the beam of rays and calculate the
      * @param light
      * @param l
